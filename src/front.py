f3477c94526dc01646beb120acf07366aa0230a7
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-22 17:23:35 +0000

    correct prediction and prepare for url img

--- src/front.py
+++ src/front.py
@@ -1,9 +1,21 @@
 import cv2
+import tensorflow as tf
+import pandas as pd
+import numpy as np
 
 def front(img_path, model):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (28,28))
+    try:
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (28,28))
 
-    prediction = model(img)
+        predicted_y = model(img)
 
-    return prediction
+        # Prepare output
+        prediction = dict()
+        prediction['best_probabilities'] = np.max(predicted_y)
+        prediction['number_selected'] = np.argmax(predicted_y)
+
+        return prediction
+
+    except Exception as e:
+        print(str(e))
