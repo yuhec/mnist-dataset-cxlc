def30ba0c60d3f3ff6179b4aeb94970572fdb558
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-28 17:37:42 +0000

    test

--- src/front.py
+++ src/front.py
@@ -11,13 +11,13 @@ def front(img_path, model):
         #img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (28,28))
 
-        #predicted_y = model(img)
+        predicted_y = model(img)
 
         # Prepare output
         #prediction = dict()
         #prediction['best_probabilities'] = np.max(predicted_y)
         #prediction['number_selected'] = np.argmax(predicted_y)
-        return 'oui'
+        return predicted_y
         #return prediction
 
     except Exception as e:
