618e0a76726179c41c7123636221d8a23a8393b0
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-28 18:48:28 +0000

    test

--- src/front.py
+++ src/front.py
@@ -15,10 +15,9 @@ def front(img_path, model):
 
         # Prepare output
         prediction = dict()
-        best_proba = np.max(predicted_y)
-        #prediction['best_probabilities'] = np.max(predicted_y)
+        prediction['best_probabilities'] = np.max(predicted_y)
         #prediction['number_selected'] = np.argmax(predicted_y)
-        return prediction
+        return 'oui'
         #return prediction
 
     except Exception as e:
