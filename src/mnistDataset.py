1d897c97a7d3b20a6272a8d6e9ed3a89e658984b
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-28 15:00:13 +0000

    test

--- src/mnistDataset.py
+++ src/mnistDataset.py
@@ -18,10 +18,10 @@ model = tf.keras.models.load_model(model_path, custom_objects={'CastToFloat32': 
 # model = load_model()
 
 def apply(input):
-    
-    if isinstance(input, str):
-        path = client.file(input).getFile().name
-        o = front(path, model)
-        return o
-    else :
-        return 'Input is not a string...'
+    return input
+    #if isinstance(input, str):
+     #   path = client.file(input).getFile().name
+      #  o = front(path, model)
+      # return o
+    #else :
+     #   return 'Input is not a string...'
