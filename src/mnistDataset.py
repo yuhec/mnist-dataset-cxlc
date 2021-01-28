f2cd3ae5dca7ce1e6a6a93dd24a017c05af3f23a
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-28 10:49:03 +0000

    load model h5

--- src/mnistDataset.py
+++ src/mnistDataset.py
@@ -1,24 +1,19 @@
 import Algorithmia
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 import autokeras as ak
+import tensorflow as tf
+from autokeras.keras_layers import CastToFloat32
 
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 
 client = Algorithmia.client()
 
-def load_model():
-    # Get file by name
-    # Open file and load model
-    file_path = 'data://.my/mnistDatasetCxlc/saved_model.pb'
-    model_path = client.file(file_path).getFile().name
-    # Open file and load model
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-        return model
+model_path = client.file("data://laetitia/mnistDatasetCxlc/my_model.h5").getFile().name
+model = tf.keras.models.load_model(model_path, custom_objects={'CastToFloat32': CastToFloat32})
 
 # model = load_model("data://.my/mnistDatasetCxlc/saved_model.pb", custom_objects=ak.CUSTOM_OBJECTS)
-model = load_model()
+# model = load_model()
 
 def apply(input):
     o = front(input, model)
