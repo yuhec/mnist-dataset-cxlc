5c6b380f291b2635ff5c59d1c7a97c8d5aa9eaeb
yuhec <laetitiaconstantin1998@gmail.com>
2021-01-28 09:22:22 +0000

    try new way to load model

--- src/mnistDataset.py
+++ src/mnistDataset.py
@@ -1,5 +1,5 @@
 import Algorithmia
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 import autokeras as ak
 
 # API calls will begin at the apply() method, with the request body passed as 'input'
@@ -7,7 +7,18 @@ import autokeras as ak
 
 client = Algorithmia.client()
 
-model = load_model("data://.my/mnistDatasetCxlc/saved_model.pb", custom_objects=ak.CUSTOM_OBJECTS)
+def load_model():
+    # Get file by name
+    # Open file and load model
+    file_path = 'data://.my/mnistDatasetCxlc/saved_model.pb'
+    model_path = client.file(file_path).getFile().name
+    # Open file and load model
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+        return model
+
+# model = load_model("data://.my/mnistDatasetCxlc/saved_model.pb", custom_objects=ak.CUSTOM_OBJECTS)
+model = load_model()
 
 def apply(input):
     o = front(input, model)
--- src/mnistDataset_test.py
+++ src/mnistDataset_test.py
@@ -1,4 +1,4 @@
 from . import mnistDataset
 
 def test_mnistDataset():
-    assert mnistDataset.apply("../data/six.png") == {'best_probabilities': 0.97026044, 'number_selected': 8}
+    assert mnistDataset.apply("data://yuhec/digits/six.png") == {'best_probabilities': 0.97026044, 'number_selected': 8}
